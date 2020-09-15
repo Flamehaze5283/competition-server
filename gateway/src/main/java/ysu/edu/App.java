08adecc2b948a2eb9efafa099cae1ecadcc5deca
halation <1124792103@qq.com>
2020-09-15 15:38:20 +0800

    hello

--- gateway/src/main/java/ysu/edu/App.java
+++ gateway/src/main/java/ysu/edu/App.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
+        System.out.println("hello");
         SpringApplication.run(App.class);
     }
 }
