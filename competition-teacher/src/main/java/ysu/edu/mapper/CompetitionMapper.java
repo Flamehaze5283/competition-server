56f8fe39e96bf0e0c12e2dbf5fe3ca6525960d51
Flamehaze5283 <214134731@qq.com>
2020-09-21 11:31:26 +0800

    更新competition表相关内容

---
+++ competition-teacher/src/main/java/ysu/edu/mapper/CompetitionMapper.java
@@ -0,0 +1,16 @@
+package ysu.edu.mapper;
+
+import ysu.edu.pojo.Competition;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zh
+ * @since 2020-09-21
+ */
+public interface CompetitionMapper extends BaseMapper<Competition> {
+
+}
---
+++ competition-teacher/src/main/java/ysu/edu/service/impl/CompetitionServiceImpl.java
@@ -0,0 +1,20 @@
+package ysu.edu.service.impl;
+
+import ysu.edu.pojo.Competition;
+import ysu.edu.mapper.CompetitionMapper;
+import ysu.edu.service.ICompetitionService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author zh
+ * @since 2020-09-21
+ */
+@Service
+public class CompetitionServiceImpl extends ServiceImpl<CompetitionMapper, Competition> implements ICompetitionService {
+
+}
